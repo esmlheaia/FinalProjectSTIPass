689b18f9d4522e6c1dc42a994664c347cb1fedbf
esmlheaia <miya.soleneee@gmail.com>
2026-05-31 17:39:44 +0800

    dashboard

--- app/src/main/java/com/example/finalprojectstipass/MainActivity.java
+++ app/src/main/java/com/example/finalprojectstipass/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.finalprojectstipass;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,54 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText etStudentId, etEmail;
+
+    private Button btnContinue;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            v.setPadding(systemBars.left, systemBars.top,
+                    systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        etStudentId = findViewById(R.id.etStudentId);
+        etEmail = findViewById(R.id.etEmail);
+        Button btnContinue = findViewById(R.id.btnContinue);
+
+        btnContinue.setOnClickListener(v -> {
+            String studentID = etStudentId.getText().toString().trim();
+            String email = etEmail.getText().toString().trim();
+
+            if (studentID.isEmpty()) {
+                etStudentId.setError("Please enter Student ID");
+                etStudentId.requestFocus();
+                return;
+            }
+
+            if (email.isEmpty()) {
+                etEmail.setError("Please enter STI Email");
+                etEmail.requestFocus();
+                return;
+            }
+
+            // Validation Logic
+            if (studentID.equals("0200012345") && email.equals("student@sti.edu.ph")) {
+                // Success
+                Toast.makeText(MainActivity.this, "Student verified successfully!", Toast.LENGTH_SHORT).show();
+                
+                Intent intent = new Intent(MainActivity.this, CreatePasswordActivity.class);
+                startActivity(intent);
+            } else {
+                // Failure
+                Toast.makeText(MainActivity.this, "Invalid Student ID or STI Email!", Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
